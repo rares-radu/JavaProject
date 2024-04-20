292d301c5c0214db3ccfd654f08991ad80ff63aa
Bintr <20655963+explowz@users.noreply.github.com>
2024-04-20 19:12:03 +0300

    Added search history

--- app/src/main/java/com/example/myapplication/SearchActivity.java
+++ app/src/main/java/com/example/myapplication/SearchActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SearchView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -9,6 +11,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SearchActivity extends AppCompatActivity
 {
+    final FixedSizeStack searchHistory = new FixedSizeStack( 4 );
+
     @Override
     protected void onCreate( Bundle savedInstanceState )
     {
@@ -21,5 +25,11 @@ public class SearchActivity extends AppCompatActivity
             v.setPadding( systemBars.left, systemBars.top, systemBars.right, systemBars.bottom );
             return insets;
         } );
+
+        SearchView searchView   = findViewById( R.id.searchView );
+        Button     searchButton = findViewById( R.id.searchButton );
+
+        // Add query to search history when the user presses the search button
+        searchButton.setOnClickListener( v -> searchHistory.push( searchView.getQuery().toString() ) );
     }
 }
