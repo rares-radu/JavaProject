7f0d1a3068da31e171e61a6f5c93566106ff2636
Bintr <20655963+explowz@users.noreply.github.com>
2024-04-20 18:42:02 +0300

    Made buttons on the main activity switch to their corresponding activity upon being clicked

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity
@@ -10,5 +13,23 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
+
+        Button searchButton = findViewById( R.id.main_button_search );
+        searchButton.setOnClickListener( this::switchToSearch );
+
+        Button quizButton = findViewById( R.id.main_button_quiz );
+        quizButton.setOnClickListener( this::switchToQuiz );
+    }
+
+    private void switchToSearch( View view )
+    {
+        Intent intent = new Intent( this, SearchActivity.class );
+        startActivity( intent );
+    }
+
+    private void switchToQuiz( View view )
+    {
+        Intent intent = new Intent( this, QuizActivity.class );
+        startActivity( intent );
     }
 }
